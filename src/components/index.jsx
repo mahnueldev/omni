107778b949dbfc2b98a1864edb4876e5210ca311
mahnueldev <emmanuelgbd3@gmail.com>
2023-07-14 13:11:06 +0100

    changes

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { default as LinkBar} from './LinkBar';
 export { default as Button} from './Button';
 export { default as SearchBar} from './SearchBar';
 export { default as Spacer} from './Spacer';
+export { default as ProfileComp} from './ProfileComp';
