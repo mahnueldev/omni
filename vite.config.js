0e070154d08be9f1bd0dcf76be82f098e1a1467a
mahnueldev <emmanuelgbd3@gmail.com>
2023-09-27 08:22:07 +0100

    vite proxy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    // Add this line
-    include: "**/*.jsx",
-  })]
-})
+  plugins: [
+    react({
+      include: "**/*.jsx",
+    }),
+  ],
+  server: {
+    proxy: {
+      '/api': 'https://test.mahnuel.com', // Replace with your API server URL
+    },
+  },
+});
